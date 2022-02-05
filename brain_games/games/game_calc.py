66535ecec8bcd675d31b27b0a841bddd7ea1d5de
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-02-05 15:23:04 +0300

    add game-calc

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,21 @@
+import random
+"""yes = even, no = odd"""
+
+
+rules = 'What is the result of the expression?'
+list_of_expressions = []
+num1 = [random.randint(0, 10) for i in range(3)]
+num2 = [random.randint(0, 10) for i in range(3)]
+rand_signs = [random.choice(['*', '+']) for i in range(3)]
+list_for_task = []
+list_for_answer = []
+for i in range(3):
+    variable = str(num1[i]) + ' ' + rand_signs[i] + ' ' + str(num2[i])
+    list_for_task.append(variable)
+    if rand_signs[i] == '*':
+        list_for_answer.append(str(num1[i] * num2[i]))
+    elif rand_signs[i] == '+':
+        answer = num1[i] + num2[i]
+        list_for_answer.append(str(num1[i] + num2[i]))
+task = list_for_task[:]
+answer = list_for_answer[:]
--- brain_games/games/logic.py
+++ brain_games/games/logic.py
@@ -1,7 +1,9 @@
+import prompt
+
+
 def game(rules, task, answer):
     print('Welcome to the Brain Games!')
-    print('May I have your name? ')
-    name = input()
+    name = prompt.string('May I have your name? ')
     print(rules)
     counter = 0
     while counter < 3:
@@ -9,9 +11,11 @@ def game(rules, task, answer):
         user_answer = input("Your answer: ")
         if user_answer == answer[counter]:
             counter = counter + 1
+            print("Correct!")
         else:
             break
     if counter == 3:
         print(f"Congratulations, {name}")
     else:
         print(f"{user_answer} is wrong(. Correct answer was {answer[counter]}.")
+        print(f"Let's try again,{name}")
