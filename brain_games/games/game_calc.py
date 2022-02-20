5e314829a33216f8f2b93f3242976e80435f3204
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-02-20 20:16:53 +0300

    move to constant values

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,4 +1,5 @@
 import random
+max_value_for_num = 10
 
 
 def get_calc_rules():
@@ -6,13 +7,14 @@ def get_calc_rules():
 
 
 def get_calc_round():
-    max_value_for_num = 10
     num1 = random.randint(0, max_value_for_num)
     num2 = random.randint(0, max_value_for_num)
-    rand_signs = random.choice(['*', '+'])
+    rand_signs = random.choice(['*', '+', '-'])
     task = str(num1) + ' ' + rand_signs + ' ' + str(num2)
     if rand_signs == '*':
         answer = str(num1 * num2)
     elif rand_signs == '+':
         answer = str(num1 + num2)
+    else:
+        answer = str(num1 - num2)
     return task, answer
